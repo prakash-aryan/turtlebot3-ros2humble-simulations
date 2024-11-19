5da1a4843fa18be66dce9625de2afc3cc56b70e5
prakash-aryan <prakasharyan25@gmail.com.com>
2024-11-19 23:17:23 +0400

    fix path issues

--- src/custom_launch/setup.py
+++ src/custom_launch/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 from glob import glob
 import os
 
@@ -7,18 +7,18 @@ package_name = 'custom_launch'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), 
-            glob(os.path.join('custom_launch', 'launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'launch'),
+            glob('custom_launch/launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='merabro',
-    maintainer_email='your_email@example.com',
+    maintainer_email='prakasharyan.2000@gmail.com',
     description='Custom launch files for TurtleBot3',
     license='Apache License 2.0',
     tests_require=['pytest'],
